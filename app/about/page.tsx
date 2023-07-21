b550e08957235a7951e940162d631e2b8d998c6e
p-friday <piotr.piiatek@gmail.com>
2023-07-21 16:31:55 +0200

    everything

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+
+export const dynamic = 'force-static';
+
+export const metadata: Metadata = {
+	title: 'About us',
+	description: 'we are a social media company',
+}
+
+export default async function About() {
+
+	return (
+		<main>
+			<h1>About</h1>
+			<p>We are a social media company!</p>
+		</main>
+	);
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavMenu from './NavMenu'
+import AuthProvider from './AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <NavMenu />
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   )
 }
